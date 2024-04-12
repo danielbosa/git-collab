946453ab02708675b8e3753ffecf635605d34eaf
AngeloC89 <Angelociulla89@gmail.com>
2024-04-12 11:18:37 +0200

    Ho creato il main!!!

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Ho creato il Main!!!</h2>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
