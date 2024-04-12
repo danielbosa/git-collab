9ec42e33ad8e80affad29137d98bf79b629a0fcc
danielbosa <danielbosa95@gmail.com>
2024-04-12 13:05:22 +0200

    Added jumbo into Main

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,13 +1,17 @@
 <template>
     <div>
         <h2>Ho creato il Main!!!!!!!</h2>
-
+        <JumboComponent/>
     </div>
 </template>
 
 <script>
+import JumboComponent from './JumboComponent.vue';
     export default {
-        name: 'HeaderComponent'
+        name: 'HeaderComponent',
+        components: {
+            JumboComponent
+        }
     }
 </script>
 
